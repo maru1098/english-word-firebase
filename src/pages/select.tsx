94b7ff00bc5b900efc98ce1a8ee64878a6777027
maru1098 <ryoga1098@icloud.com>
2021-04-06 00:51:45 +0900

    💄 単語帳選択ページのUI更新

--- src/pages/select.tsx
+++ src/pages/select.tsx
@@ -19,13 +19,16 @@ const Select = () => {
   }, [currentUser]);
 
   return (
-    <div>
-      <h1>単語帳選択ページ</h1>
-      <ul className="list-disc">
+    <div className="flex flex-col justify-between bg-gray-100 ">
+      <h1 className="mx-auto my-10 px-20 py-3 border-4 border-green-500 text-3xl bg-green-300">
+        単語帳選択
+      </h1>
+      <ul className="mx-auto ">
         {folder.map((val, i) => {
           return (
             <li
               key={i}
+              className=" px-24 py-14 h-10 text-center text-3xl flex flex-col justify-center border border-b border-gray-500 bg-gray-300 sm:cursor-pointer hover:bg-gray-200"
               onClick={async () => {
                 router.push({
                   pathname: "/card",
@@ -39,7 +42,7 @@ const Select = () => {
         })}
       </ul>
       <Link href="/">
-        <button className="mx-auto mb-14 w-32 h-10 rounded-full shadow bg-green-300 sm:hover:bg-green-400">
+        <button className="mx-auto my-14 w-32 h-10 rounded-full shadow bg-green-300 sm:hover:bg-green-400">
           ホームへ
         </button>
       </Link>
